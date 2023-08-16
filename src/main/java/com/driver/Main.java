6c9b18a166a666ce2b5a5a7f9c015024a54d0865
siddHacks <siddtheprofessional@gmail.com>
2023-08-16 22:15:07 +0530

    encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+        RWOnly ro = new RWOnly();
+        ro.setName("Dravid");
+        System.out.println(ro.getName());
+    }
+
 }
